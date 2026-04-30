8d9f222959e0b137d0c8f28b96eac755ec7e3487
MaroHuh <marohuhh@gmail.com>
2026-04-30 16:29:24 +0300

    fixes

---
+++ js/search.js
@@ -0,0 +1,12 @@
+const searchBar = document.getElementById("search-bar");
+
+searchBar.addEventListener("input", function () {
+  const q = this.value.toLowerCase().trim();
+  filteredData = PRODUCTS.filter(
+    //change filteredData to only include products matching the search
+    (p) =>
+      p.id.toLowerCase().includes(q)
+  );
+  currentPage = 1;
+  render(); //calling the render function to update the changes to the cards
+});
