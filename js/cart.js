e3b10872dfc3a7e7b9ebca9a7037a64228e40944
MaroHuh <marohuhh@gmail.com>
2026-05-04 23:41:20 +0300

    fixing cart form

--- js/cart.js
+++ js/cart.js
@@ -9,7 +9,8 @@ function updateTotals() {
     cart.forEach(p => {
         subtotal += p.price * p.quantity;
     });
-    const shipping = 20;
+    const shipping = cart.length > 0 ? 20 : 0;
+    document.querySelector(".shipping_price").textContent = "$" + shipping;
     document.querySelector(".subtotal_checkout").textContent = "$" + subtotal;
     document.querySelector(".total_checkout").textContent = "$" + (subtotal + shipping);
 }
@@ -66,3 +67,41 @@ items.addEventListener("click", function(e) {
 });
 
 renderCart();
+document.querySelector(".myform").addEventListener("submit", function(e) {
+  e.preventDefault();
+  let inputs = document.querySelectorAll(".input_info input");
+  let formStatus = document.getElementById("form-status");
+  let isValid = true;
+
+  inputs.forEach((input) => {
+    let warning = input.nextElementSibling; 
+
+    if (input.value.trim() === "") {
+      warning.classList.remove("hidden");
+      warning.style.color = "red";
+      input.style.border = "2px solid red";
+      isValid = false;
+    } else {
+      warning.classList.add("hidden");
+      input.style.border = "1px solid #ccc";
+    }
+  });
+
+  if (isValid) {
+    if(cart.length === 0){
+        formStatus.classList.remove("hidden");
+        formStatus.innerHTML =` Cart is empty`;
+        formStatus.style.color = "red";
+    }
+    else{
+    formStatus.classList.remove("hidden");
+    formStatus.innerHTML = `Success! Order is on it's way.`;
+    formStatus.style.color = "green";
+    cart = [];
+    document.querySelector(".myform").reset();
+    localStorage.setItem("cart", JSON.stringify(cart));
+    renderCart();
+    }
+  }
+
+});
